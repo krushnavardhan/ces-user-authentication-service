f6fa059cfdce02f1c0f98a01ee114b8655726f6b
krushnavardhan <62050470+krushnavardhan@users.noreply.github.com>
2025-09-01 22:52:10 +0530

    Removed unwanted code.

--- src/main/java/com/as/config/SecurityConfiguration.java
+++ src/main/java/com/as/config/SecurityConfiguration.java
@@ -1,24 +1,14 @@
 package com.as.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
-	@Autowired
-	private JwtAuthenticationFilter jwtAuthFilter;
-
-	@Autowired
-	private AuthenticationProvider authenticationProvider;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,13 +20,7 @@ public class SecurityConfiguration {
 			.requestMatchers("/auth-service/auth/**")
 			.permitAll()
 			.anyRequest()
-			.authenticated()
-			.and()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.authenticationProvider(authenticationProvider)
-			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+			.authenticated();
 			
 		return http.build();
 	}
